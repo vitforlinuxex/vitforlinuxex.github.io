a7586d7dd665f37250f41b3e271ac35b6861f25c
vitforlinuxex <237251197+vitforlinuxex@users.noreply.github.com>
2026-03-11 16:05:22 +0100

    Update search.js

--- assets/js/search.js
+++ assets/js/search.js
@@ -1,1 +1,16 @@
+    <script>
+        function searchContent() {
+            // Prende il valore della ricerca e lo converte in minuscolo
+            let input = document.getElementById('searchBar').value.toLowerCase();
+            let items = document.getElementsByClassName('middle');
 
+            for (let i = 0; i < items.length; i++) {
+                // Controlla se il testo dell'elemento contiene la stringa cercata
+                if (items[i].innerText.toLowerCase().includes(input)) {
+                    items[i].classList.remove('hidden'); // Mostra
+                } else {
+                    items[i].classList.add('hidden'); // Nasconde
+                }
+            }
+        }
+    </script>
