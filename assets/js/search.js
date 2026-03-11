1875664e5f2b2e7b07887f956151918425724b22
vitforlinuxex <237251197+vitforlinuxex@users.noreply.github.com>
2026-03-11 16:06:30 +0100

    Update search.js

--- assets/js/search.js
+++ assets/js/search.js
@@ -2,7 +2,7 @@
         function searchContent() {
             // Prende il valore della ricerca e lo converte in minuscolo
             let input = document.getElementById('searchBar').value.toLowerCase();
-            let items = document.getElementsByClassName('middle');
+            let items = document.getElementsByClassName('searchable');
 
             for (let i = 0; i < items.length; i++) {
                 // Controlla se il testo dell'elemento contiene la stringa cercata
